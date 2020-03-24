b8178338b928ca5a96b176ce46bd393e16b9ecdc
Lyle <494205736@qq.com>
2020-03-24 22:29:35 +0800

    login页面初始

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@
 </template>
 
 <style lang="less">
+@import url("//at.alicdn.com/t/font_1426139_jm7n2373nlh.css");
 * {
   margin: 0;
   padding: 0;
---
+++ src/views/Login.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    登录页
+    <span class="iconfont iconnew"></span>
+    <span class="iconfont iconweixin"></span>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
