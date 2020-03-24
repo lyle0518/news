b1fd41547c77dc5c5d115fb12dd6b7ed1824092b
Lyle <494205736@qq.com>
2020-03-24 23:43:33 +0800

    logo完成

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,14 +1,50 @@
 <template>
-  <div>
-    登录页
-    <span class="iconfont iconnew"></span>
-    <span class="iconfont iconweixin"></span>
+  <div class="box">
+    <div class="head"></div>
+    <div class="logo">
+      <span class="iconfont iconnew iconlogo"></span>
+      <span class="iconfont iconicon-test test"></span>
+    </div>
   </div>
 </template>
 
 <script>
-export default {};
 </script>
 
 <style>
+@media screen and (width: 750px) {
+  /* 把750分成10份，每一份是75，就让75作为一个rem */
+  html {
+    font-size: 36px;
+  }
+}
+@media screen and (width: 360px) {
+  /* 把375分成10份，每一份就是37.5，这是一个rem */
+  html {
+    font-size: 36px;
+  }
+}
+.box {
+  width: 10rem;
+}
+.head {
+  width: 10rem;
+  height: 0.666667rem;
+  background-color: #757575;
+}
+.logo {
+  position: relative;
+  text-align: center;
+}
+.logo .iconlogo {
+  font-size: 3.452055rem;
+  width: 3.452055rem;
+  color: red;
+  margin: 0 auto;
+}
+.logo .test {
+  position: absolute;
+  top: 7px;
+  left: 7px;
+}
 </style>
