fb3fb789f6892c8f3657bd036290b3b38d7ae60f
Lyle <494205736@qq.com>
2020-03-27 22:32:03 +0800

    '路由守卫完成'

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 
 Vue.use(VueRouter);
+// 添加全局首位
 
 const routes = [
   {
@@ -22,6 +23,19 @@ const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
   routes
+});
+router.beforeEach((to, from, next) => {
+  if (to.path === "/personal") {
+    const userJson = JSON.parse(localStorage.getItem("userInfo")) || [];
+    if (userJson.token) {
+      next();
+    } else {
+      next("/login");
+    }
+  } else {
+    //非进入个人中心页面
+    next();
+  }
 });
 
 export default router;
