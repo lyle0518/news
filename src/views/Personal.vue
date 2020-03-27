749db3a1f6f04a99be9585ea1a821966a1b8da64
Lyle <494205736@qq.com>
2020-03-27 21:19:53 +0800

    '增加导航栏'

--- src/views/Personal.vue
+++ src/views/Personal.vue
@@ -1,28 +1,38 @@
 <template>
   <div class="container">
+    <!-- 顶部导航条 -->
+    <div class="nav">
+      <span class="iconfont iconjiantou2"></span>
+      <strong>个人中心</strong>
+      <span class="iconfont iconshouye"></span>
+    </div>
     <div class="header">
       <!-- 头像 -->
       <div class="img">
-        <img src="https://www.baidu.com/img/bd_logo1.png" alt />
+        <!-- <img :src="'http://127.0.0.1:3000'+userInfo.head_img" alt /> -->
+        <img :src="$axios.defaults.baseURL + userInfo.head_img" alt />
       </div>
       <!-- 姓名 -->
       <div class="profile">
         <div class="avatar">
-          <span class="iconfont iconxingbienan nan"></span>
-          广州新闻
+          <span class="iconfont iconxingbienan" v-if="userInfo.gender === 1"></span>
+          <span class="iconfont iconxingbienv" v-if="userInfo.gender === 0"></span>
+          {{ userInfo.nickname }}
         </div>
-        <p>2020-03-27</p>
+        <p>{{ moment(userInfo.create_date).format("YYYY-MM-DD") }}</p>
       </div>
       <!-- 右侧箭头 -->
       <span class="iconfont iconjiantou1"></span>
     </div>
     <!-- 列表按钮-->
-    <Listbar v-for="(item,index) in rows" :label="item.label" :tips="item.tips" :key="index"></Listbar>
+    <Listbar v-for="(item, index) in rows" :label="item.label" :tips="item.tips" :key="index"></Listbar>
   </div>
 </template>
 
 <script>
 import Listbar from "@/components/Listbar";
+// 引入时间处理工具库
+import moment from "moment";
 export default {
   data() {
     return {
@@ -44,7 +54,8 @@ export default {
           tips: ""
         }
       ],
-      userInfo: {}
+      userInfo: {},
+      moment
     };
   },
   // 注册组件Listbar
@@ -71,7 +82,18 @@ export default {
 };
 </script>
 
-<style scoped lang='less'>
+<style scoped lang="less">
+.nav {
+  padding: 20/ 360 * 100vw;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  line-height: 10 / 360 * 100vw;
+  border: 1px solid #eee;
+  .iconshouye {
+    font-size: 20px;
+  }
+}
 .header {
   padding: 20 / 360 * 100vw;
   display: flex;
@@ -91,6 +113,12 @@ export default {
   padding-left: 20 / 360 * 100vw;
   p {
     color: #999;
+  }
+  .iconxingbienan {
+    color: blue;
+  }
+  .iconxingbienv {
+    color: palevioletred;
   }
 }
 </style>
