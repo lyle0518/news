bc47c1a76a63272d56dfd8c2cf790c8bb75d36bf
Lyle <494205736@qq.com>
2020-03-30 00:08:21 +0800

    '编辑个人信息新版本-增加修改密码回退'

--- src/views/Edit.vue
+++ src/views/Edit.vue
@@ -1,195 +1,187 @@
 <template>
   <div>
-    <!-- 导航栏 -->
-    <Nav title="编辑信息" @click.native="handleBack(true)"></Nav>
+    <Nav title="编辑资料"></Nav>
     <!-- 头像 -->
-    <div class="userImg">
+    <div class="headImg">
       <img :src="$axios.defaults.baseURL+user.head_img" alt />
       <van-uploader :after-read="afterRead" />
     </div>
-    <Listbar label="昵称" :tips="user.nickname" @click.native="handleNickname"></Listbar>
-    <!-- 添加编辑昵称的弹窗 -->
-    <!-- confirm点击确认按钮触发 -->
+    <Listbar label="昵称" :tips="user.nickname" @click.native="showNickname=true"></Listbar>
     <van-dialog
-      v-model="shownickname"
-      title="标题"
+      v-model="showNickname"
+      title="修改昵称"
       show-cancel-button
       @confirm="handleChangeNickname"
+      @cancel="handleCancelNickname"
     >
-      <input type="text" placeholder="修改昵称" v-model="nickname" />
+      <van-field v-model="nickName" placeholder="请输入用户名" />
     </van-dialog>
-    <Listbar label="密码" tips="******" @click.native="handlePassword"></Listbar>
-    <!-- 修改密码弹窗 -->
+    <Listbar label="密码" tips="******" @click.native="showpassword=true"></Listbar>
     <van-dialog
       v-model="showpassword"
-      title="标题"
+      title="修改密码"
       show-cancel-button
       @confirm="handleChangepassword"
     >
-      <input type="password" placeholder="修改密码" v-model="password" />
+      <!-- 旧密码: -->
+      <!-- <van-field v-model="password" />新密码: -->
+      <van-field type="password" v-model="password" placeholder="请输入新密码" />
     </van-dialog>
-    <Listbar label="性别" :tips="['女','男'][gender]" @click.native="handleGender"></Listbar>
-    <!-- 性别上拉框 -->
+    <Listbar label="性别" :tips="['女','男'][user.gender]" @click.native="showGender=true"></Listbar>
     <van-action-sheet v-model="showGender" :actions="actions" @select="onSelect" />
   </div>
 </template>
 
 <script>
 import Nav from "@/components/Nav";
-// 引入LIstbar按钮组件
 import Listbar from "@/components/Listbar";
 export default {
   data() {
     return {
-      //axios请求回来的数据
-      user: {},
-      //本地的数据
       userInfo: {},
-      shownickname: false,
+      user: {},
+      showNickname: false,
       showpassword: false,
       showGender: false,
-      nickname: "",
-      gender: "",
+      nickName: "",
       password: "",
       actions: [
-        { name: "男", value: 1 },
-        { name: "女", value: 0 }
+        { name: "女", value: 0 },
+        { name: "男", value: 1 }
       ]
     };
   },
   mounted() {
     const userInfo = JSON.parse(localStorage.getItem("userInfo"));
+    // 将这个对象存到data中用于反显
+    console.log(userInfo);
     this.userInfo = userInfo;
-    console.log(this.userInfo);
-
     this.$axios({
       url: "/user/" + userInfo.user.id,
       headers: {
         Authorization: userInfo.token
       }
     }).then(res => {
+      // 解析获取的参数用于反显
       const { data } = res.data;
-      console.log(data);
+      // 存入data参数中
       this.user = data;
-      //   存储请求返回的的nickname值
-      this.nickname = data.nickname;
-      //   存储请求返回的的gender值
-      this.gender = data.gender;
+      this.nickName = this.user.nickname;
+      // 防止在弹出框显示密码
+      // this.password = this.user.password;
     });
-    // console.log(this.user);
   },
-
   methods: {
-    handleBack(back) {
-      console.log(back);
-      if (back) {
-        this.$router.back();
-      }
-    },
     afterRead(file) {
       // 此时可以自行将文件上传至服务器
-      //   从本地拿token值
-
-      console.log(this.userInfo);
-      //   console.log(file);
+      console.log(file);
       const fd = new FormData();
       fd.append("file", file.file);
       this.$axios({
         url: "/upload",
         method: "post",
+        data: fd,
         headers: {
           Authorization: this.userInfo.token
-        },
-        data: fd
+        }
       }).then(res => {
-        // this.$toast.success("修改头像成功");
-        console.log(res);
-        // 解析出图片的地址
+        // 获取上传后的图片在数据库中的文件地址
         const { url } = res.data.data;
-        this.user.head_img = url;
+        // 发送请求修改数据库数据
         this.handleEdit({ head_img: url });
+        // 修改上传后返显的数据,此时数据是页面一加载请求后的user中的地址,
+        this.user.head_img = url;
+        console.log(res);
       });
     },
-    // 封装修改数据库请求
-    handleEdit(data) {
-      this.$axios({
-        //获取id错误
-        url: "/user_update/" + this.userInfo.user.id,
-        method: "post",
-        headers: {
-          Authorization: this.userInfo.token
-        },
-        data
-      }).then(res => {
-        this.$toast.success("资料修改成功");
+    // 修改密码轻提示的回调函数
+    changePassword() {
+      this.$toast.success("信息修改成功");
+      this.$toast.success({
+        message: "信息修改成功",
+        duration: 1000,
+        onClose: () => {
+          this.$notify({
+            message: "请重新登录",
+            duration: 1000,
+            onClose: () => {
+              localStorage.removeItem("userInfo");
+              this.$router.replace("/login");
+            }
+          });
+        }
       });
     },
-    // 点击修改nickname的show值
-    handleNickname() {
-      // 值为布尔值
-      this.shownickname = true;
+    // 修改其他业务成功轻提示的回调函数
+    changeOther() {
+      this.$toast.success("信息修改成功");
     },
 
-    //点击确认按钮触发函数
+    handleEdit(data, fn) {
+      this.$axios({
+        url: "/user_update/" + this.user.id,
+        headers: {
+          Authorization: this.userInfo.token
+        },
+        method: "post",
+        data
+      }).then(fn);
+    },
+
     handleChangeNickname() {
-      //   console.log(this.nickname);
-      // 发送请求
-      this.handleEdit({ nickname: this.nickname });
-      //   把this.nickname修改到本地内存
-      this.user.nickname = this.nickname;
+      this.handleEdit({ nickname: this.nickname }, this.changeOther);
+      // 修改昵称后更改第一次获取数据返显的user中的nickname,做数据在本地的更新
+      this.user.nickname = this.nickName;
     },
-    // 修改密码弹窗显示
-    handlePassword() {
-      this.showpassword = true;
+    handleCancelNickname() {
+      // 取消按钮时输入框中的默认昵称改回原样
+      this.nickName = this.user.nickname;
     },
-    // 点击确认发送修改密码请求
+    // 修改密码
     handleChangepassword() {
-      this.handleEdit({ password: this.password });
-      //   修改加载完成请求的user里的密码值,为修改密码如果需要显示当前密码做准备
-      this.user.password = this.password;
+      this.handleEdit({ password: this.password }, this.changePassword);
     },
-    // 弹出选择性别框
-    handleGender() {
-      this.showGender = true;
-    },
-    // 选择性别时触发的事件
     onSelect(item) {
       // 默认情况下点击选项时不会自动收起
       // 可以通过 close-on-click-action 属性开启自动收起
       this.showGender = false;
-      console.log(item.value);
+      // 修改数据库数据
       this.handleEdit({ gender: item.value });
-      //   页面的性别是从用户信息接口返回的需要修改data中的user.gender
-      this.gender = item.value;
+      // 修改user中返显的gender值
+      this.user.gender = item.value;
     }
   },
+  // 修改数据库数据
+
   components: {
-    Listbar,
-    Nav
+    Nav,
+    Listbar
   }
+  //   页面加载完成发送请求获取数据渲染到页面
+  // 获取本地的token和id值
 };
 </script>
 
-<style scoped lang='less'>
-.userImg {
+<style lang='less' scoped>
+.headImg {
   position: relative;
   display: flex;
   justify-content: center;
   align-items: center;
-  padding: 20 / 360 * 100vw;
+  padding: 40 / 360 * 100vw;
+
   img {
-    width: 100 / 360 * 100vw;
-    height: 100 / 360 * 100vw;
+    width: 100 /360 * 100vw;
+    height: 100 /360 * 100vw;
     border-radius: 50%;
   }
   .van-uploader {
     position: absolute;
-    padding: 0;
     width: 100 /360 * 100vw;
     height: 100 /360 * 100vw;
     top: 50%;
     left: 50%;
-    transform: translateX(-50 / 360 * 100vw) translateY(-50 / 360 * 100vw);
+    transform: translateX(-50 /360 * 100vw) translateY(-50 /360 * 100vw);
     opacity: 0;
   }
 }
